aa25242d26ab0097c929ce4e8bd743a473da6323
Kenneth Posey <kenneth.posey@gmail.com>
2014-07-08 15:31:28 -0700

    Added an example about indexing

--- FsharpTutorial/ConsoleApplication1/Program.fs
+++ FsharpTutorial/ConsoleApplication1/Program.fs
@@ -1,7 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+open System
+open System.Threading
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    "Enter text>> " |> Console.Write
+    let maininput = Console.ReadLine()
+    printfn "%A" maininput
+    Thread.Sleep(2000)
     0 // return an integer exit code
