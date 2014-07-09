571a0b10862b10790f9543e73925ca5418207da1
Kenneth Posey <kenneth.posey@gmail.com>
2014-07-09 10:03:29 -0700

    Writing real code

--- FsharpTutorial/ConsoleApplication1/Program.fs
+++ FsharpTutorial/ConsoleApplication1/Program.fs
@@ -4,10 +4,64 @@
 open System
 open System.Threading
 
+module TileTypes = 
+    type Orientation = 
+        | Vertical = 0
+        | Horizontal = 1
+    
+    type Terrain = 
+        | Water = 0
+        | Rock = 1
+        | Land = 2
+
+    type Shipinfo = 
+        {
+            Length : int;
+            Width : int;
+            Direction : Orientation
+        }
+
+    type Shiptype =
+        | Frigate of Info : Shipinfo
+        | Destroyer of Info : Shipinfo
+        | Cruiser of Info : Shipinfo
+        | Battlecruiser of Info : Shipinfo
+        | Carrier of Info : Shipinfo
+
+    type gridSquare = 
+        | Ship of Kind : Shiptype
+        | Terrain of Kind : Terrain 
+
+
+open TileTypes
 [<EntryPoint>]
-let main argv = 
-    "Enter text>> " |> Console.Write
-    let maininput = Console.ReadLine()
-    printfn "%A" maininput
-    Thread.Sleep(2000)
+let main pArgumentValueArray = 
+
+    let grid = [| for x in 1 .. 11 ->
+                    gridSquare.Terrain
+                    |]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     0 // return an integer exit code
