ee99f4afaf4fe7b81004cbd4db673d50bf830409
Kenneth-Posey <kenneth.posey@gmail.com>
2014-07-14 22:17:56 -0700

    Added bitmap detector

---
+++ FsharpTutorial/ConsoleApplication1/imagesearch.fs
@@ -0,0 +1,13 @@
+﻿open System
+open System.Drawing
+open System.Drawing.Imaging
+
+module imagesearch = 
+    let searchBitmap (pSmallBitmap:Bitmap) (pLargeBitmap:Bitmap) = 
+        let tSmallRectangle = Rectangle(0, 0, pSmallBitmap.Width, pSmallBitmap.Height)
+        let tLargeRectangle = Rectangle(0, 0, pLargeBitmap.Width, pLargeBitmap.Height)
+        
+        let tLockMode = ImageLockMode.ReadOnly
+        let tImageFormat = PixelFormat.Format24bppRgb
+
+        let tSmallBitmapData = pSmallBitmap.LockBits(tSmallRectangle, tLockMode, tImageFormat)
