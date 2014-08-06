87da3a922b89d4d426d49f21494e023ea8202319
Kenneth Posey <kenneth.posey@gmail.com>
2014-08-05 17:12:19 -0700

    Continued progress on tail recursion

--- FsharpTutorial/YeFsharpLibrary/FsharpImaging.fs
+++ FsharpTutorial/YeFsharpLibrary/FsharpImaging.fs
@@ -49,56 +49,53 @@ module ImageSearch =
         let tSmallArray = Transform2D <| LoadBitmapIntoArray pSmallBitmap 
         let tLargeArray = Transform2D <| LoadBitmapIntoArray pLargeBitmap
         
-        let tSearchWidth = pLargeBitmap.Width - pSmallBitmap.Width - 1          // Account for 0 index
-        let tSearchHeight = pLargeBitmap.Height - pSmallBitmap.Height - 1       // Account for 0 index
+        let SearchWidth = pLargeBitmap.Width - pSmallBitmap.Width - 1          // Account for 0 index
+        let SearchHeight = pLargeBitmap.Height - pSmallBitmap.Height - 1       // Account for 0 index
 
-        let mutable tHeightIndex = 0
-        let mutable tWidthIndex = 0
-        let mutable tMatch = false
-        let mutable tContinue = true
+        // let mutable tHeightIndex = 0
+        // let mutable tWidthIndex = 0
+        // let mutable tMatch = false
+        // let mutable tContinue = true
+        
+        let tFirstSmallPixel = tSmallArray.[0].[0]
 
         let WidthLoop pHeightIndex pWidthIndex pWidthContinue =
             let rec WidthLoopRec pHeightIndex pWidthIndex pWidthContinue =
-                if pWidthIndex < tSearchWidth && pWidthContinue then
-                    1, true, true
-                else
-                    0, false, false
+                match ( pWidthIndex < SearchWidth , pWidthContinue ) with
+                | ( true, true ) -> 1, true, true
+                | ( _ , _ )      -> 0, false, false
             WidthLoopRec pHeightIndex 0 true
 
-        let HeightLoop pHeightIndex pHeightContinue =
+        let HeightLoop () =
             let rec HeightLoopRec pHeightIndex pHeightContinue =
-                let tWidthIndex, tWidthContinue, tFoundImage = 
-                    match ((pHeightIndex < tSearchHeight) , pHeightContinue ) with
-                    | (   _    , true  ) -> WidthLoop pHeightIndex 0 true // Image not found, continue
-                    // There is no way for there to be a (true, true) state
-                    // so it's equivalent to pattern match with a wildcard
-                    // to ensure complete matching
+                let WidthIndex, WidthContinue, FoundImage = 
+                    match ( pHeightIndex < SearchHeight , pHeightContinue ) with
+                    | ( _     , true  ) -> WidthLoop pHeightIndex 0 true // Image not found, continue
                     | ( true  , false ) -> 0, false, false // Image found, don't continue
                     | ( false , false ) -> 0, false, false // Image not found, don't continue
 
-                match (tWidthContinue, tFoundImage) with
+                match (WidthContinue, FoundImage) with
                 | ( true  , _     ) -> HeightLoopRec ( pHeightIndex + 1 ) true
-                | ( false , true  ) -> true, tWidthIndex, pHeightIndex
-                | ( false , false ) -> false, tWidthIndex, pHeightIndex
+                | ( false , true  ) -> true, WidthIndex, pHeightIndex
+                | ( false , false ) -> false, WidthIndex, pHeightIndex
                 
             HeightLoopRec 0 true    // pHeightIndex pContinue
 
         do HeightLoop |> ignore
 
-        let tFirstSmallPixel = tSmallArray.[0].[0]
-        while ( tHeightIndex < tSearchHeight - 1 ) && tContinue do
-            while ( tWidthIndex < tSearchWidth - 1 ) && tContinue do
-                let tCurrentValue = tLargeArray.[tHeightIndex].[tWidthIndex]
-
-                if tCurrentValue = tFirstSmallPixel then
-                    tMatch <- ArrayFunctions.SearchSubset tSmallArray tLargeArray ( tHeightIndex, tWidthIndex )
-                    if tMatch then tContinue <- false
-
-                if tMatch = false && tContinue = true then
-                    tWidthIndex <- tWidthIndex + 1
-
-            if tMatch = false && tContinue = true then
-                tWidthIndex  <- 0                       // Reset to search next row
-                tHeightIndex <- tHeightIndex + 1
-
-        tMatch, tWidthIndex, tHeightIndex
+        // while ( tHeightIndex < tSearchHeight - 1 ) && tContinue do
+        //     while ( tWidthIndex < tSearchWidth - 1 ) && tContinue do
+        //         let tCurrentValue = tLargeArray.[tHeightIndex].[tWidthIndex]
+        // 
+        //         if tCurrentValue = tFirstSmallPixel then
+        //             tMatch <- ArrayFunctions.SearchSubset tSmallArray tLargeArray ( tHeightIndex, tWidthIndex )
+        //             if tMatch then tContinue <- false
+        // 
+        //         if tMatch = false && tContinue = true then
+        //             tWidthIndex <- tWidthIndex + 1
+        // 
+        //     if tMatch = false && tContinue = true then
+        //         tWidthIndex  <- 0                       // Reset to search next row
+        //         tHeightIndex <- tHeightIndex + 1
+        // 
+        // tMatch, tWidthIndex, tHeightIndex
