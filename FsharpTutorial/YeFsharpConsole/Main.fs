701af0eb474e3ef057dc7d093572735064fbe5db
Kenneth Posey <kenneth.posey@gmail.com>
2014-08-19 15:28:15 -0700

    Progress checkin

--- FsharpTutorial/YeFsharpConsole/Main.fs
+++ FsharpTutorial/YeFsharpConsole/Main.fs
@@ -4,8 +4,8 @@ module Main =
 
     open FsharpImaging
     open Testing
-    open System.Drawing
     open EveOnline
+    open System.Drawing
     
     [<EntryPoint>]
     let main (args:string[]) = 
@@ -27,12 +27,31 @@ module Main =
             use tLargeBitmap = new Bitmap("testimage1.bmp")
         
             let tSuccess, xCoord, yCoord = ImageSearch.SearchBitmap tSmallBitmap tLargeBitmap
-
+            
             ()
         with
             | _ -> ()
 
-        let stttrnig = MarketParser.LoadItemsFromUrl("http://eve-files.com/chribba/typeid.txt")
+        let FilterByName (name:string) (tuple:(string * string)[]) =
+            tuple
+            |> Array.filter (fun (x, y) -> y.Contains(name))
+
+        let FilterOreOnly (tuple:(string * string)[]) = 
+            tuple
+            |> Array.filter (fun (x, y) -> y.Contains("Blue") = false)
+            |> Array.filter (fun (x, y) -> y.Contains("Processing") = false)
+            |> Array.filter (fun (x, y) -> y.Contains("Mining") = false)
+            
+        let itemArray = MarketParser.LoadTypeIdsFromUrl EveData.TypeIdUrl
+
+        let tritItems  = itemArray |> FilterOreOnly |> FilterByName "Tritanium"
+        let veldItems  = itemArray |> FilterOreOnly |> FilterByName "Veldspar"
+        let scordItems = itemArray |> FilterOreOnly |> FilterByName "Scordite"
+        let pyroItems  = itemArray |> FilterOreOnly |> FilterByName "Pyroxeres"
+        let kernItems  = itemArray |> FilterOreOnly |> FilterByName "Kernite"
+        let hemItems   = itemArray |> FilterOreOnly |> FilterByName "Hemorphite"
+        let hedItems   = itemArray |> FilterOreOnly |> FilterByName "Hedbergite"
+        let jaspItems  = itemArray |> FilterOreOnly |> FilterByName "Jaspet"
 
         // Must return from function
         0
--- FsharpTutorial/YeFsharpLibrary/MarketParser.fs
+++ FsharpTutorial/YeFsharpLibrary/MarketParser.fs
@@ -1,35 +1,126 @@
 ﻿namespace EveOnline
 
+module EveData = 
+    type SystemName =
+        | Jita    = 30000142
+        | Dodixie = 30002659
+        | Amarr   = 30002187
+        | Hek     = 30002053
+        | Rens    = 30002510
+
+    type MarketAPIRequest = {
+        typeid      : int
+        usesystem   : int
+        regionlimit : int
+        sethours    : int
+        setminQ     : int
+        }
+
+    let TypeIdUrl   = "http://eve-files.com/chribba/typeid.txt"
+    let QuickLook   = "http://api.eve-central.com/api/quicklook"
+
+    module RawMaterials =    
+        type Minerals =
+        | Tritanium = 34
+        | Pyerite   = 35
+        | Mexallon  = 36
+        | Isogen    = 37
+        | Nocxium   = 38
+        | Zydrine   = 39
+        | Megacyte  = 40
+
+        type Veldspar =
+        | Default      = 1230
+        | Concentrated = 17470
+        | Dense        = 17471
+
+        type CompVeldspar =
+        | Default      = 28430
+        | Concentrated = 28431
+        | Dense        = 28432
+        
+        type Scordite =
+        | Default   = 1228
+        | Condensed = 17463
+        | Massive   = 17464
+
+        type CompScordite =
+        | Default   = 28427
+        | Condensed = 28428
+        | Massive   = 28429
+
+        type Pyroxeres =
+        | Default   = 1224
+        | Solid     = 17459
+        | Viscous   = 17460
+
+        type CompPyroxeres =
+        | Default   = 28424
+        | Solid     = 28425
+        | Viscous   = 28426
+        
+        type Hedbergite =
+        | Default   = 21
+        | Vitric    = 17440
+        | Glazed    = 17441
+
+        type CompHedbergite = 
+        | Default   = 28400
+        | Vitric    = 28401
+        | Glazed    = 28402
+
+        type Hemorphite = 
+        | Default   = 1231
+        | Vivid     = 17444
+        | Radiant   = 17445
+
+        type CompHemorphite = 
+        | Default   = 18403
+        | Vivid     = 28404
+        | Radiant   = 28405
+
+        type Jaspet = 
+        | Default   = 1226
+        | Pure      = 17448
+        | Pristine  = 17449
+
+        type CompJaspet = 
+        | Default   = 28406
+        | Pure      = 28407
+        | Pristine  = 28408
+
+                    
+
 module MarketParser =
     open System
     open System.Text.RegularExpressions
     open System.Net
 
-    let LoadUrl url = 
+    let LoadUrl (url:string) = 
         use client = new WebClient()
         client.DownloadString( new Uri(url) )
 
     let ParseItem (text:string) (regex:Regex) =
         let parsed = regex.Match text
-
         match parsed.Success with
-        | true -> parsed.Groups.[1].Value.Trim() , parsed.Groups.[2].Value.Trim()
+        | true  -> parsed.Groups.[1].Value.Trim() , parsed.Groups.[2].Value.Trim()
         | false -> "" , ""
+        // If it is unable to match then it returns 
+        // an empty tuple to be filtered out later
 
     let IsNotEmpty (x:string , y:string) =
         (x.Length > 0) && (y.Length > 0)
+        
+    let FilterEmpty (text:(string * string)[]) =
+        text |> Array.filter (fun (x, y) -> IsNotEmpty (x, y))
 
     let SplitOnNewline (text:string) (regex:Regex) =
-        let splitted = text.Split [|'\n'|]
-
-        Array.filter (fun (x, y) -> IsNotEmpty (x, y))
-        <| [| // Filters out "empty" tuples (non-matches)
-            for line in splitted do
+        [| 
+            for line in text.Split [|'\n'|] do
                 yield ParseItem (line.TrimEnd()) regex
         |] 
 
-    let LoadItemsFromUrl url =
-        let itemText = LoadUrl url
-        let regex = new Regex "([0-9]{1,9})[ ]{6}([\w '.-_]*)"
-
-        SplitOnNewline itemText regex
+    let LoadTypeIdsFromUrl (url:string) =
+        new Regex "([0-9]{1,9})[ ]{6}([\w '.-_]*)"
+        |> SplitOnNewline (LoadUrl url)
+        |> FilterEmpty
