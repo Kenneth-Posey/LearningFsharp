d03237b70abd1781abf468111f9a5a63b30196d2
Kenneth Posey <kenneth.posey@gmail.com>
2014-09-29 13:56:07 -0700

    Start C# interop classes

---
+++ FsharpTutorial/YeFsharpLibrary/Interop/CollectionProvider.fs
@@ -0,0 +1,10 @@
+﻿namespace Collections 
+
+module Provider = 
+    open EveOnline
+
+    type OreList () = 
+        member this.OreNames = EveData.Collections.RawOreNames
+
+    type IceList () =
+        member this.IceNames = EveData.Collections.RawIceNames
