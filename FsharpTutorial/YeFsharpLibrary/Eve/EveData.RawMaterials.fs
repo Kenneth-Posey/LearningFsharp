8952ae4d3a092b1d7738537878aa48b95c2acde6
Kenneth Posey <kenneth.posey@gmail.com>
2014-12-04 16:56:43 -0800

    figuring out how to pack values into DUs

--- FsharpTutorial/YeFsharpLibrary/Eve/EveData.RawMaterials.fs
+++ FsharpTutorial/YeFsharpLibrary/Eve/EveData.RawMaterials.fs
@@ -81,14 +81,19 @@ module RawMaterials =
         StrontiumClathrates = 0
     }
 
-    type MaterialName = MaterialName of string
-    type IsCompressed = IsCompressed of bool
-    type Volume = Volume of single
-    type BaseId = BaseId of int
+    type MaterialName = string
+    type IsCompressed = Raw | Compressed
+    type Volume = single
+    type TypeId   = int
+    type TypeId5  = int
+    type TypeId10 = int
+    type Name   = string
+    type Name5  = string
+    type Name10 = string
 
     type Yield = 
-    | OreYield of OreYield
-    | IceYield of IceYield
+    | OreYield of Yield : OreYield
+    | IceYield of Yield : IceYield
 
     type RefinedProduct = 
     | Mineral of Mineral
@@ -103,55 +108,77 @@ module RawMaterials =
     | IsCompressed
     | IsNotCompressed
 
-    type Ore = 
-    | Arkonor
-    | Bistot
-    | Crokite
-    | DarkOchre
-    | Gneiss
-    | Hedbergite
-    | Hemorphite
-    | Jaspet
-    | Kernite
-    | Mercoxit
-    | Omber
-    | Plagioclase
-    | Pyroxeres
-    | Scordite
-    | Spodumain
-    | Veldspar
-
-    type Ice = 
-    | BlueIce
-    | ClearIcicle
-    | DarkGlitter
-    | EnrichedClearIcicle
-    | Gelidus
-    | GlacialMass
-    | GlareCrust
-    | Krystallos
-    | PristineWhiteGlaze
-    | SmoothGlacialMass
-    | ThickBlueIce
-    | WhiteGlaze
-
     type RawMaterial = {
-        MaterialName    : MaterialName
         IsCompressed    : IsCompressed
         Yield           : Yield
         Volume          : Volume
-        BaseId          : BaseId
+        TypeId          : TypeId
+        Name            : Name
     }
 
     type RawOre = {        
         RawMaterial : RawMaterial
-        GetBase5    : unit -> int
-        GetBase10   : unit -> int
-        GetName5    : unit -> int
-        GetName10   : unit -> int
+        TypeId5     : TypeId5
+        TypeId10    : TypeId10
+        Name5       : Name5
+        Name10      : Name10
     }
 
+    type RawIce = {
+        RawMaterial : RawMaterial
+    }
 
+    type Ore = 
+    | Arkonor     of Ore : RawOre
+    | Bistot      of Ore : RawOre
+    | Crokite     of Ore : RawOre
+    | DarkOchre   of Ore : RawOre
+    | Gneiss      of Ore : RawOre
+    | Hedbergite  of Ore : RawOre
+    | Hemorphite  of Ore : RawOre
+    | Jaspet      of Ore : RawOre
+    | Kernite     of Ore : RawOre
+    | Mercoxit    of Ore : RawOre
+    | Omber       of Ore : RawOre
+    | Plagioclase of Ore : RawOre
+    | Pyroxeres   of Ore : RawOre
+    | Scordite    of Ore : RawOre
+    | Spodumain   of Ore : RawOre
+    | Veldspar    of Ore : RawOre
+    
+    type Ice = 
+    | BlueIce             of Ice : RawIce
+    | ClearIcicle         of Ice : RawIce
+    | DarkGlitter         of Ice : RawIce
+    | EnrichedClearIcicle of Ice : RawIce
+    | Gelidus             of Ice : RawIce
+    | GlacialMass         of Ice : RawIce
+    | GlareCrust          of Ice : RawIce
+    | Krystallos          of Ice : RawIce
+    | PristineWhiteGlaze  of Ice : RawIce
+    | SmoothGlacialMass   of Ice : RawIce
+    | ThickBlueIce        of Ice : RawIce
+    | WhiteGlaze          of Ice : RawIce
+
+    let TestBlueIce = {
+        RawMaterial = {
+            IsCompressed = IsCompressed.Raw
+            Yield = Yield.IceYield({
+                BaseIceYield with
+                HeavyWater = 100
+            })
+            Volume = 100.0f
+            TypeId = 10000
+            Name = "BlueIce"
+        }
+    }
+
+    let blerhg = 
+        match TestBlueIce.RawMaterial.Yield with 
+        | IceYield x -> x 
+        | OreYield _ -> BaseIceYield
+
+    let ThisIs100 = blerhg.HeavyWater
 
     type IRawMat<'T> = 
         abstract member GetName   : unit -> string
