33b75e814e35c9813587168e444863e760048a76
Kenneth Posey <kenneth.posey@gmail.com>
2015-01-08 16:08:12 -0800

    add basic items to load

--- FsharpTutorial/YeFsharpConsole/dddmain.fs
+++ FsharpTutorial/YeFsharpConsole/dddmain.fs
@@ -8,13 +8,26 @@ module Main =
     open EveOnline.MarketDomain.Market
 
     [<EntryPoint>]
-    let main (args:string[]) = 
+    let main (_) = 
         
         let result = loadItems Jita <| 
             [
                 IceProduct IceProduct.HeavyWater
                 IceProduct IceProduct.HeliumIsotopes
+                IceProduct IceProduct.HydrogenIsotopes
+                IceProduct IceProduct.LiquidOzone
+                IceProduct IceProduct.NitrogenIsotopes
+                IceProduct IceProduct.OxygenIsotopes
+                IceProduct IceProduct.StrontiumClathrates
 
+                Mineral Mineral.Isogen
+                Mineral Mineral.Megacyte
+                Mineral Mineral.Mexallon
+                Mineral Mineral.Morphite
+                Mineral Mineral.Nocxium
+                Mineral Mineral.Pyerite
+                Mineral Mineral.Tritanium
+                Mineral Mineral.Zydrine
             ]
 
 
