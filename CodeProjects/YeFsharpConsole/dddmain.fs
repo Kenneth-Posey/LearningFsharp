11e56a78ef754478416a0ac0fd0d0db717e3775a
Kenneth Posey <kenneth.posey@gmail.com>
2015-01-23 14:41:53 -0800

    add code to drive new function in console

--- CodeProjects/YeFsharpConsole/dddmain.fs
+++ CodeProjects/YeFsharpConsole/dddmain.fs
@@ -3,6 +3,7 @@
 open Format.Text
 
 module Main = 
+    open EveOnline.ProductDomain.Types
     open EveOnline.MarketDomain.Types
     open EveOnline.MarketDomain.Records
     open EveOnline.MarketDomain.Providers
@@ -17,18 +18,19 @@ module Main =
         let iceProductPrices = getPrice (RefinedProduct.IceProduct) 
         let mineralPrices = getPrice (RefinedProduct.Mineral)
         let refineValue x y = string (GetRefineValue x y).Value
+        let vol = Volume 1000.0f
 
         for ice in IceList do
+            let value = sprintf "%0.2f" (GetVolumePrice vol iceProductPrices IsNotCompressed ice).Value
             System.Console.WriteLine ( "Ice " + (Name ice).Value
-                + " with value: " 
-                + refineValue (GetYield ice) (iceProductPrices)
+                + " with value: " + value
             )
             
             
         for ore in OreList do
+            let value = sprintf "%0.2f" (GetVolumePrice vol mineralPrices IsNotCompressed ore).Value
             System.Console.WriteLine ( "Ore " + (Name ore).Value
-                + " with value: "
-                + refineValue (GetYield ore) (mineralPrices)
+                + " with value: " + value
             )
 
         0 
