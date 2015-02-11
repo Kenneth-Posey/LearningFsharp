647814f51e3d20838784abd28722b198715d96e0
Kenneth-Posey <kenneth.posey@gmail.com>
2015-02-10 17:47:35 -0800

    add random walk and monogame

---
+++ CodeProjects/Monogame/Game1.fs
@@ -0,0 +1,14 @@
+﻿namespace Monogame
+
+module Game1 = 
+    open Microsoft.Xna.Framework
+    open Microsoft.Xna.Framework.Graphics
+
+    type Game1 () as this = 
+        inherit Game ()
+
+        abstract member graphics : GraphicsDeviceManager
+        abstract member spriteBatch : SpriteBatch
+
+        member this.graphics = new GraphicsDeviceManager (this)
+        member this.spriteBatch = new SpriteBatch (this.graphics)
---
+++ CodeProjects/Monogame/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+namespace Monogame
+
+module Program = 
+    open Game1
+
+        
+    type Program () = 
+        [<EntryPoint>]
+        let main argv = 
+            use game = new Game1()
+            printfn "%A" argv
+            0 // return an integer exit code
