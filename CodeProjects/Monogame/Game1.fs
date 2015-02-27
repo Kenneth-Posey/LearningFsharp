6d378dd99ab324705ea7195d7ed62acb6aa7179d
Kenneth-Posey <kenneth.posey@gmail.com>
2015-02-26 18:06:26 -0800

    basic monogame work

--- CodeProjects/Monogame/Game1.fs
+++ CodeProjects/Monogame/Game1.fs
@@ -2,13 +2,21 @@
 
 module Game1 = 
     open Microsoft.Xna.Framework
+    open Microsoft.Xna.Framework.Content
     open Microsoft.Xna.Framework.Graphics
 
     type Game1 () as this = 
         inherit Game ()
 
-        abstract member graphics : GraphicsDeviceManager
-        abstract member spriteBatch : SpriteBatch
 
-        member this.graphics = new GraphicsDeviceManager (this)
-        member this.spriteBatch = new SpriteBatch (this.graphics)
+        do 
+            let contentmanager = new Content.ContentManager ()
+            // this.alien <- 
+
+        abstract member alien : Texture2D
+
+        // abstract member graphics : GraphicsDeviceManager
+        // abstract member spriteBatch : SpriteBatch
+        // 
+        // member this.graphics = new GraphicsDeviceManager (this)
+        // member this.spriteBatch = new SpriteBatch (this.graphics)
